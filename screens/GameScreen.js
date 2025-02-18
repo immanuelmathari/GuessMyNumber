f72872f3a47470f8dee2200e1c1c9a33be4f9235
Immanuel Mathari <immanuelnganga2001@gmail.com>
2025-02-18 09:16:39 +0300

    4.70 Working with Icons (Button Icons) 17/02 eg Ionicons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -6,7 +6,8 @@ import NumberContainer from "../components/game/NumberContainer";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
-
+import Ionicons from '@expo/vector-icons/Ionicons';
+ 
 function generateRandomBetween(min, max, exclude) {
     const randomNumber = Math.floor(Math.random() * (max - min)) + min;
     
@@ -79,11 +80,15 @@ function GameScreen({userNumber, onGameOver})
                 <View style={styles.buttonsContainer}>
                     <View style={styles.buttonContainer}>
 
-                <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+                <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+                    <Ionicons name="remove-sharp" size={24} color="white" />
+                </PrimaryButton>
                     </View>
                     <View style={styles.buttonContainer}>
 
-                <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+                <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+                    <Ionicons name="add" size={24} color="white" />
+                </PrimaryButton>
                     </View>
                 </View>
                 {/* + - */}
